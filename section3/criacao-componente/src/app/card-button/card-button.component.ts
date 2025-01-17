143ca9c8e90bffbf030592e6134ff8dc70497d73
Matheus Arend <i_arend@hotmail.com>
2025-01-17 10:56:11 -0300

    29. Template dentro do componente (prop "template)
    É possível criar um componente sem criar o arquivo html:
    ng g c nome-componente --inline-template

--- section3/criacao-componente/src/app/card-button/card-button.component.ts
+++ section3/criacao-componente/src/app/card-button/card-button.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button',
   standalone: false,
-  
-  templateUrl: './card-button.component.html',
+  template: `
+    <div class="card-button">Testando</div>
+  `,
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
