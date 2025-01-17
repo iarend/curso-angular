6dfabf42e395109e5335328a407938b7a3f70517
Matheus Arend <i_arend@hotmail.com>
2025-01-17 09:38:07 -0300

    Aula 26

--- section3/criacao-componente/src/app/app.module.ts
+++ section3/criacao-componente/src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
-import { CardButtonComponent } from './card-button/card-button.component';
-import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CardRoxoComponent,
-    CardButtonComponent,
-    CardRoxoButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
