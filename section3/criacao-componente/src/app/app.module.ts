70b357c04dabb97d073ad7ddfc289f41ca5f6c8c
Matheus Arend <i_arend@hotmail.com>
2025-01-17 09:31:12 -0300

    Aula 25

--- section3/criacao-componente/src/app/app.module.ts
+++ section3/criacao-componente/src/app/app.module.ts
@@ -7,18 +7,19 @@ import { CardComponent } from './card/card.component';
 import { CardRoxoComponent } from './card-roxo/card-roxo.component';
 import { CardButtonComponent } from './card-button/card-button.component';
 import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
+import { CardsModule } from './cards/cards.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
     CardRoxoComponent,
     CardButtonComponent,
     CardRoxoButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardsModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
