47041d746cb5608d85611c8fa41cf0b2c4d7868a
Matheus Arend <i_arend@hotmail.com>
2025-01-19 09:47:44 -0300

    41. View Encapsulation Emulated

--- section3/criacao-componente/src/app/card-roxo/card-roxo.component.ts
+++ section3/criacao-componente/src/app/card-roxo/card-roxo.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card-roxo',
   standalone: false,
   
   templateUrl: './card-roxo.component.html',
-  styleUrl: './card-roxo.component.scss'
+  styleUrl: './card-roxo.component.scss',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class CardRoxoComponent {
 
