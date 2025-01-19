2037112ffdb7d9360cd1276a9ca4a7014eb6ae58
Matheus Arend <i_arend@hotmail.com>
2025-01-19 09:45:08 -0300

    40. View Encapsulation None

--- section3/criacao-componente/src/app/card/card.component.ts
+++ section3/criacao-componente/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 interface IPlan{
   infos: IInfos;
@@ -14,7 +14,8 @@ interface IInfos{
   standalone: false,
   
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class CardComponent implements OnInit {
 
